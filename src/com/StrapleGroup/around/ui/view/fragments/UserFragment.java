16c38ef4470bc3601a6e49a8e656d80e55fc314d
robert-StrapleGroup <robert@straplegroup.com>
2014-09-26 19:33:20 +0200

    working bottom navig buttons

--- src/com/StrapleGroup/around/ui/view/fragments/UserFragment.java
+++ src/com/StrapleGroup/around/ui/view/fragments/UserFragment.java
@@ -5,7 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import com.StrapleGroup.around.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 /**
  * Created by Robert on 2014-09-19.
@@ -14,6 +19,12 @@ public class UserFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View pView = inflater.inflate(R.layout.fragment_user, container, false);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext()).build());
+        DisplayImageOptions pOptions = new DisplayImageOptions.Builder().displayer(new RoundedBitmapDisplayer(330)).build();
+        ImageView pProfileView = (ImageView) pView.findViewById(R.id.profilePhoto);
+        imageLoader.displayImage("drawable://" + R.drawable.facebook_example, pProfileView, pOptions);
+        return pView;
     }
 }
